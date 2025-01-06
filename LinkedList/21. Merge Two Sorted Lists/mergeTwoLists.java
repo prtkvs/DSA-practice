ff37cb30e2fac74973bef761f65cdc6667ff8700
Prateek Verma <prateek222rithik@gmail.com>
2025-01-06 12:27:43 +0530

    delete duplicates

---
+++ LinkedList/21. Merge Two Sorted Lists/mergeTwoLists.java
@@ -0,0 +1,12 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+    }
+}
