9335d7e8d3405d0bb8f3a1faec7f9f7e95fb5c63
Prateek Verma <prateek222rithik@gmail.com>
2025-01-08 09:53:24 +0530

    sort list

--- LinkedList/148. Sort List/sortList.java
+++ LinkedList/148. Sort List/sortList.java
@@ -1,47 +1,110 @@
+// TC - O(n) not optimised solution check merge sort solution on leetcode
+class LL {
 
-   class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+    private Node head;
+    private Node tail;
+    private int size;
 
-   class Solution {
-       public ListNode sortList(ListNode head) {
-           if (head == null || head.next == null) {
-               return head;
-           }
-           ListNode prev = head;
-           ListNode fast = head;
-           ListNode slow = head;
-           while (fast != null && fast.next != null) {
-               fast = fast.next.next;
-               prev = slow;
-               slow = slow.next;
-           }
-           prev.next = null;
-           ListNode s = sortList(head);
-           ListNode b = sortList(slow);
-           ListNode dummy = new ListNode(0);
-           ListNode cur = dummy;
-           while (s != null && b != null) {
-               if (s.val > b.val) {
-                   cur.next = b;
-                   b = b.next;
-               } else {
-                   cur.next = s;
-                   s = s.next;
-               }
-               cur = cur.next;
-           }
+    public LL() {
+        this.size = 0;
+    }
 
-           if (s != null) {
-               cur.next = s;
-           }
-           if (b != null) {
-               cur.next = b;
-           }
-           return dummy.next;
-       }
-   }
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void bubbleSort() {
+        bubbleSort(size - 1, 0);
+    }
+
+    private void bubbleSort(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            Node first = get(col);
+            Node second = get(col + 1);
+
+            if (first.value > second.value) {
+                // swap
+                if (first == head) {
+                    head = second;
+                    first.next = second.next;
+                    second.next = first;
+                } else if (second == tail) {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    tail = first;
+                    first.next = null;
+                    second.next = tail;
+                } else {
+                    Node prev = get(col - 1);
+                    prev.next = second;
+                    first.next = second.next;
+                    second.next = first;
+                }
+            }
+            bubbleSort(row, col + 1);
+        } else {
+            bubbleSort(row - 1, 0);
+        }
+    }
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+    public static void main(String[] args) {
+        LL list = new LL();
+        for (int i = 7; i > 0; i--) {
+            list.insertLast(i);
+        }
+        list.display();
+        list.bubbleSort();
+        list.display();
+
+    }
+}
