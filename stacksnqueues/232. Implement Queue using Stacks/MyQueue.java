b9fbe98b0782ab7f63540300714b15fed6fdd773
Prateek Verma <prateek222rithik@gmail.com>
2025-01-16 01:04:37 +0530

    implement queue using two stacks

--- stacksnqueues/232. Implement Queue using Stacks/MyQueue.java
+++ stacksnqueues/232. Implement Queue using Stacks/MyQueue.java
@@ -1,2 +1,44 @@
-public class MyQueue {
+import java.util.Stack;
+class MyQueue {
+    Stack<Integer> first ;
+    Stack<Integer> second;
+    public MyQueue() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+
+    public void push(int x) {
+        first.push(x);
+    }
+
+    public int pop() {
+        if(second.empty()){
+            while(!first.empty()){
+                second.push(first.pop());
+            }
+        }
+        return second.pop();
+    }
+
+    public int peek() {
+        if(second.empty()){
+            while(!first.empty()){
+                second.push(first.pop());
+            }
+        }
+        return second.peek();
+    }
+
+    public boolean empty() {
+        return first.empty() && second.empty();
+    }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
